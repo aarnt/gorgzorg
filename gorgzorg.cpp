e648f838972174a83c3e3656e64eb045585c6b2a
Alexandre Arnt <qtgzmanager@gmail.com>
2021-02-12 11:13:45 -0300

    - Added header in source files.

--- gorgzorg.cpp
+++ gorgzorg.cpp
@@ -1,3 +1,23 @@
+/*
+* This file is part of GorgZorg, a simple CLI network file transfer tool.
+* Copyright (C) 2021 Alexandre Albuquerque Arnt
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
+
 #include "gorgzorg.h"
 
 #include <QDataStream>
@@ -313,7 +333,7 @@ void GorgZorg::startServer(const QString &ipAddress)
 
   if (!m_server->listen(QHostAddress(ip), m_port))
   {
-    //If we could not bind in this port...
+    //If we could not bind to this port...
     qout << QString("ERROR: Port %1 is already being used in this host!").arg(m_port) << Qt::endl;
     exit(1);
   }
